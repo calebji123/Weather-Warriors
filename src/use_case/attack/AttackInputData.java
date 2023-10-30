efa62b1cfeef5c6ef56a5d9141bf66a47bd48b15
Derekliaow <derekliaow.li@mail.utoronto.ca>
2023-10-30 10:28:49 -0400

    Wrote attack use cases and started interactor

--- src/use_case/attack/AttackInputData.java
+++ src/use_case/attack/AttackInputData.java
@@ -1,4 +1,14 @@
 package use_case.attack;
 
 public class AttackInputData {
+    final private String attack;
+    final private String specialAttack;
+
+
+    public AttackInputData(String attack, String specialAttack) {
+        this.attack = attack;
+        this.specialAttack = specialAttack;
+    }
+    public String getAttack() {return this.attack;}
+    public String getSpecial() {return this.specialAttack;}
 }
--- src/use_case/attack/AttackOutputData.java
+++ src/use_case/attack/AttackOutputData.java
@@ -1,4 +1,13 @@
 package use_case.attack;
 
 public class AttackOutputData {
+    private final Integer damage;
+    private Boolean cardDefeated;
+
+
+    public AttackOutputData(Integer damage, Boolean cardDefeated) {
+        this.damage = damage;
+        this.cardDefeated = cardDefeated;
+    }
+    public Integer getDamage() {return this.damage;}
 }
