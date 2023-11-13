3f4aa409e9909884d789c6c040ae6d261c6ed954
Derekliaow <derekliaow.li@mail.utoronto.ca>
2023-11-13 17:43:53 -0500

    Derek: Completed classes/interfaces in use_case.swap except the DAO (waiting on group decisions about implementation).

--- src/use_case/swap/SwapOutputData.java
+++ src/use_case/swap/SwapOutputData.java
@@ -1,4 +1,24 @@
 package use_case.swap;
 
+import entity.Card;
+
 public class SwapOutputData {
+    private final String message;
+    private String nextCard;
+    private Integer activeCardHealth;
+    private Integer bossHealth;
+
+    public SwapOutputData(String message, Integer activeCardHealth, Integer bossHealth) {
+        this.message = message;
+        this.activeCardHealth = activeCardHealth;
+        this.bossHealth = bossHealth;
+    }
+
+    public String getMessage() {return this.message;}
+    public String getNextCard() {return this.nextCard;}
+    public void setNextCard(String name) {this.nextCard = name;}
+    public Integer getActiveCardHealth() {return this.activeCardHealth;}
+    public void setActiveCardHealth(Integer health) {this.activeCardHealth = health;}
+    public Integer getBossHealth() {return this.bossHealth;}
+    public void setBossHealth(Integer health) {this.bossHealth = health;}
 }
