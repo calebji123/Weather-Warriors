54e493a03c65c235e6bda156f64a5af5adfd51dc
Joseph Duraisingh <josephduraisingh@gmail.com>
2023-11-13 09:59:38 -0500

    How to play data access

---
+++ src/data_access/HowToPlayDataAccess.java
@@ -0,0 +1,21 @@
+package data_access;
+import java.io.*;
+public class HowToPlayDataAccess {
+    private String outputText = "";
+    public HowToPlayDataAccess() throws IOException{
+        String filepath = "src/data_files/HowToPlay";
+        File howToPlay = new File(filepath);
+        try(BufferedReader reader = new BufferedReader(new FileReader(howToPlay))){
+            StringBuilder output = new StringBuilder();
+            String row;
+            while ((row = reader.readLine()) != null){
+                output.append(row);
+            }
+            this.outputText = output.toString();
+        }
+    }
+
+    public String getOutputText() {
+        return outputText;
+    }
+}
