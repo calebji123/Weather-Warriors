9626f1cfc0dc99ed64cf01d2b3f1ce19ad1f8810
Joseph Duraisingh <josephduraisingh@gmail.com>
2023-11-13 10:11:02 -0500

    Initial commit for entities

--- src/entity/Card.java
+++ src/entity/Card.java
@@ -8,14 +8,14 @@ public class Card {
     private boolean isDead;
     private boolean isActive;
     private boolean isNext;
-    public Card(String Name, Attack rAttack, Attack sAttack, Integer hp){
+    public Card(String Name, int rAttack, int sAttackCode, Integer hp){
         this.CardName = Name;
-        this.regularAttack = rAttack;
-        this.specialAttack = sAttack;
+        this.specialAttack = new Attack(rAttack, sAttackCode);
         this.isActive = false;
         this.isNext = false;
         this.isDead = false;
         this.HP = hp;
+        this.regularAttack = new Attack(rAttack);
     }
 
 
@@ -29,5 +29,6 @@ public class Card {
     public void setHP (int dmg){this.HP -= dmg;}
     public void die(){this.isDead = true;}
     public void activate(){this.isActive = true;}
+    public void deactivate(){this.isActive = false;}
     public void makeNext(){this.isNext = true;}
 }
