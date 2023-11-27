01de072cbce78722ab25657f8088c65edbe3bdec
Joseph Duraisingh <josephduraisingh@gmail.com>
2023-11-26 23:33:09 -0500

    Start use case updated files

--- src/interface_adaptor/start/StartViewModel.java
+++ src/interface_adaptor/start/StartViewModel.java
@@ -1,4 +1,32 @@
 package interface_adaptor.start;
 
-public class StartViewModel {
+import interface_adaptor.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class StartViewModel extends ViewModel {
+    private StartState state;
+    public StartViewModel(){
+        super("start_turn");
+    }
+
+    public StartState getState(){
+        return state;
+    }
+
+    public void setState(StartState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
