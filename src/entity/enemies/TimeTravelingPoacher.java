cd7b9180d9aa8695e32a06446ece13b79fbe3586
Joseph Duraisingh <josephduraisingh@gmail.com>
2023-12-04 09:47:46 -0500

    Updating cards for special attack

--- src/entity/enemies/TimeTravelingPoacher.java
+++ src/entity/enemies/TimeTravelingPoacher.java
@@ -10,32 +10,35 @@ import java.util.List;
 import java.util.Random;
 //make this an abstract class then hardcode in bosses I think
 public class TimeTravelingPoacher implements Opponent {
-    private List<Attack> bossMoves = new ArrayList<>();
-    private Attack lastMove;
-    private Integer HP = 1000;
+    private List<SpecialAttack> bossMoves = new ArrayList<>();
+    private SpecialAttack lastMove;
+    private Integer HP = 500;
     private final String imageFilePath = ".jpg";
 
 
 
-    public TimeTravelingPoacher(Location location){
-        Attack pass = new SpecialAttack(0, 1, location);
+    public TimeTravelingPoacher(){
+        SpecialAttack pass = new SpecialAttack(0, 0);
         this.bossMoves.add(pass);
-        Attack heal = new SpecialAttack(1, 1, location);
-        this.bossMoves.add(heal);
-        Attack bigAttack = new Attack(20);
+        this.bossMoves.add(pass);
+        SpecialAttack bigAttack = new SpecialAttack(20, 0);
         this.bossMoves.add(bigAttack);
-        Attack smallAttack = new Attack(5);
+        this.bossMoves.add(bigAttack);
+        SpecialAttack smallAttack = new SpecialAttack(5,0);
         this.bossMoves.add(smallAttack);
-        Attack fireball = new SpecialAttack(1, 1, location);
+        this.bossMoves.add(smallAttack);
+        SpecialAttack fireball = new SpecialAttack(10, 13);
         this.bossMoves.add(fireball);
-        Attack iceStorm = new SpecialAttack(1, 1, location);
+        SpecialAttack iceStorm = new SpecialAttack(10, 63);
         this.bossMoves.add(iceStorm);
-        Attack gustBlast = new SpecialAttack(1, 1, location);
-        this.bossMoves.add(gustBlast);
+//        SpecialAttack gustBlast = new SpecialAttack(10, 1);
+  //      this.bossMoves.add(gustBlast);
+        SpecialAttack waterFall = new SpecialAttack(10, 23);
+        this.bossMoves.add(waterFall);
     }
-    public Attack getAttack(){
+    public SpecialAttack getAttack(){
         Random rand = new Random();
-        Attack atk = bossMoves.get(rand.nextInt(bossMoves.size()));
+        SpecialAttack atk = bossMoves.get(rand.nextInt(bossMoves.size()));
         this.lastMove = atk;
         return atk;
     }
